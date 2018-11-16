b35d644df544276d6b107222a0ad907504ed83d4
Bluegameryt295 <42852697+Bluegameryt295@users.noreply.github.com>
2018-11-16 05:51:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2155,4 +2155,30 @@ message.guild.members.forEach(m => {
  
 });
 
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc2")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
