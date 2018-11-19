e1233f433a6e201f0c83d80269b9a7b0e1388652
Bluegameryt295 <42852697+Bluegameryt295@users.noreply.github.com>
2018-11-19 22:50:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-var prefix = "#"
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
