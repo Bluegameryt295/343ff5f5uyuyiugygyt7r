5ac24f3f802341732ec8b309ced4dc70b8ed549f
Bluegameryt295 <42852697+Bluegameryt295@users.noreply.github.com>
2018-11-22 17:20:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '#'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`BlueBot Codes.`,"https://www.twitch.tv/S-F")
+client.user.setGame(`Blue Codes.™`,"https://www.twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
